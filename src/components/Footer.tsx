8b0aac2870e2b622f4e036b8e4bcd6b5507e488e
otenevictor <solomonotene36@gmail.com>
2023-07-29 16:21:18 +0100

    footer-update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,18 +4,19 @@ import { Icon } from "@iconify/react";
 import { Link as RounterLink } from "react-router-dom";
 
 const Footer = () => {
+
   return (
     <>
-      <div className="container-fluid text-center mt-5 blue_bg pt-4" >
+      <div className="container-fluid mt-5 blue_bg pt-4" >
         <div className="row g-2">
           <div className="col">
             <div className="p-3"></div>
           </div>
-          <div className="col-2">
+          <div className="col-2 p-2 logo_image">
             <Link className="ms-5" to="/">
-              <img src={Logo} alt="logo" className=" md:ml-40  w-20 " />
+              <img src={Logo} alt="logo" className="img-fluid " />
             </Link>
-            <div className="p-2 m-3">
+            <div className="p-2 m-3 d-flex justify-content-end">
               <RounterLink to="/" target="_blank" className="g-3">
                 <Icon icon="mdi:twitter" width="25" />
               </RounterLink>
@@ -30,84 +31,118 @@ const Footer = () => {
               </RounterLink>
             </div>
           </div>
-          <div className="col-2">
+          <div className="col-2 container-fluid">
             <div className="p-0">
               <b>
-                <p className="m-0">Why Scissor ? </p>
+                <p className="mb-3 ">Why Scissor ? </p>
               </b>
-              <p className="m-0">Scissor 101</p>
-              <p className="m-0 fs-6">Integrations & API</p>
-              <p>Pricing </p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li className="">Scissor 101</li>
+              <li className="m-0 ">Integrations & API</li>
+              <li>Pricing </li>
+              </ul>
             </div>
           </div>
           <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0">Solutions</p>
+                <p className="mb-3 ">Solutions</p>
               </b>
-              <p className="m-0 fs-6">
-                Social Media Digital Marketing Customer Service For Developers
-              </p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li >
+                Social Media 
+              </li>
+              <li>Digital Marketing</li>
+              <li>Customer Service</li>
+              <li>  For Developers</li>
+              </ul>
             </div>
           </div>
           <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0">Products</p>
+                <p className="mb-3 ">Products</p>
               </b>
-              <p className="m-0 fs-6">Link Management QR Codes Link-in-bio</p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li >Link Management </li>
+              <li> QR Codes</li>
+              <li> Link-in-bio</li>
+              </ul>
+
             </div>
           </div>
-          <div className="col-1">
+          <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0">Company</p>
+                <p className="mb-3 ">Company</p>
               </b>
-              <p className="m-0 fs-6">
-                About Scissor Careers Partners Press Contact Reviews
-              </p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li className="m-0 ">
+                About Scissor  
+              </li>
+              <li>Careers</li>
+              <li>Partners </li>
+              <li>Press </li>
+              <li>Contact</li>
+              <li> Reviews</li>
+
+              </ul>
             </div>
           </div>
           <div className="col">
-            <div className="p-3"></div>
+            <div className="p-0"></div>
           </div>
         </div>
         <div className="row g-2">
-          <div className="col">
-            <div className="p-3"></div>
+          <div className="col-1">
+            <div className="p-0"></div>
           </div>
           <div className="col-2"></div>
           <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0">Resources</p>
+                <p className="mb-3 ">Resources</p>
               </b>
-              <p className="m-0 fs-6">
-                Blog Resource Library Developers App Connectors Support Trust
-                Center Browser Extension Mobile App
-              </p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li className="m-0 ">
+                Blog   
+              </li>
+              <li>Resource Library </li>
+              <li>Developers</li>
+              <li>App Connectors</li>
+              <li>Support </li>
+              <li>Trust Center </li>
+              <li>Browser Extension </li>
+              <li>Mobile App</li>
+              </ul>
             </div>
           </div>
           <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0">Features</p>
+                <p className="mb-3 ">Features</p>
               </b>
-              <p className="m-0 fs-6">
-                Branded Links Mobile Links Campaign Management & Analytics QR
-                Code generation{" "}
-              </p>
+              <ul className="text-start p-0"  style={{listStyleType: "none"}}>
+              <li className="m-0">Branded Links </li>
+              <li> Mobile Links</li>
+              <li>Campaign </li>
+              <li>Management & Analytics </li>
+              <li>QR Code generation{" "}</li>
+              </ul>
             </div>
           </div>
           <div className="col-2">
             <div className="p-0">
               <b>
-                <p className="m-0 ">Legal</p>
+                <p className="mb-3 ">Legal</p>
               </b>
-              <p className="m-0 fs-6">
-                Privacy Policy Cookie Policy Terms of Service Acceptable Use
-                Policy Code of Conduct
-              </p>
+              <ul className="text-start p-0" style={{listStyleType: "none"}}>
+              <li className="m-0 "> Privacy Policy </li>
+              <li>Cookie Policy </li>
+              <li>Terms of Service </li>
+              <li>Acceptable Use Policy </li>
+              <li>Code of Conduct</li>
+              </ul>
             </div>
           </div>
           <div className="col-1">
